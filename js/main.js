c586497acdb33d8b10311d0f97afeeb60f87ada2
mara06161-ux <mara06161@gmail.com>
2026-06-13 21:30:05 +0200

    JS : dark mode avec localStorage, navbar dynamique au scroll

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+const btn = document.getElementById("themeToggle");
 
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+        btn.textContent = "☀️";
+    } else {
+        btn.textContent = "🌙";
+    }
+});
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        btn.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        btn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+});
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+    }
+});
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
